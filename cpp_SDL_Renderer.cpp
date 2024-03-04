56efa5d9ba955fb152e903e7bd781b839fc5dc55
Nevemlaci <nevemlaci@proton.me>
2024-03-04 23:44:39 +0100

    added comments

--- cpp_SDL_Renderer.cpp
+++ cpp_SDL_Renderer.cpp
@@ -1,5 +1,7 @@
 #include "cpp_SDL_Renderer.hpp"
 
+//
+
 namespace SDL {
 	Renderer::Renderer(const Window& p_window, int index, Uint32 flags)
 		: window(p_window), sdlrenderer(SDL_CreateRenderer(p_window.GetWindow(), index, flags)), insertmode(src), nextrect({0, 0, 0, 0}) {
