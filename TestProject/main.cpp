72fb1afa9cd6f6676d206c701fc43873884f2819
Nevemlaci <nevemlaci@proton.me>
2024-03-05 14:52:20 +0100

    test project

---
+++ TestProject/main.cpp
@@ -0,0 +1,21 @@
+#include <cpp_SDL_include.hpp>
+#undef main
+
+int main() {
+	SDL::SDL& sdl = SDL::SDL::Get();
+	SDL::Window window("asd", 400, 400, 500, 500, 0);
+	bool isRunning = true;
+	const SDL_Event& event = sdl.GetEvent();
+	while (isRunning) {
+		sdl.PollEvents();
+		switch (event.type) {
+		case SDL_QUIT:
+			isRunning = false;
+			break;
+		default: 
+			break;
+		}
+		
+	}
+	return 0;
+}
